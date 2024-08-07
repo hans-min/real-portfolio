66d9f723b2543dee279ce8bf744bb9f96272bc6c
Hanji <kaitothedraggy@gmail.com>
2024-08-07 12:01:02 +0200

    Updates

---
+++ _app/immutable/chunks/paths.CN5T4Hvq.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1l5iejc)==null?void 0:s.base)??"/real-portfolio";var e;const l=((e=globalThis.__sveltekit_1l5iejc)==null?void 0:e.assets)??a;export{l as a,a as b};
---
+++ _app/immutable/entry/start.BMrOD-s_.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Gnk5qCqZ.js";export{t as start};
